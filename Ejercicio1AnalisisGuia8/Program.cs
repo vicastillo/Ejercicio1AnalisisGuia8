48351fa41222b9679ef1ed2734a1f77af27f3c46
vicastillo <vicente468@outlook.com>
2021-03-03 03:12:50 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1AnalisisGuia8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1AnalisisGuia8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.Title = "Calcular edad";
+
+            int edad, año;
+            Console.WriteLine("Por favor ingrese su año de nacimiento:");
+            año = int.Parse(Console.ReadLine());
+
+            edad = DateTime.Today.Year - año;
+
+            Console.WriteLine("Usted tiene " + edad + " años de edad.");
+
+            Console.ReadKey();
+        }
+    }
+}
